113052703d88db8dedebb216365f188153cebb9d
silentfl <e.mazgalev@gmail.com>
2017-02-24 02:47:37 +0300

    Add CreateReportJob spec

--- spec/jobs/create_report_job_spec.rb
+++ spec/jobs/create_report_job_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CreateReportJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "uploads a backup" do
+    ActiveJob::Base.queue_adapter = :test
+    expect {
+      CreateReportJob.perform_later(
+        Faker::Internet.email,
+        (Time.now - 1.day).to_s,
+        Time.now.to_s
+      )
+    }.to have_enqueued_job
+  end
 end
