4cceb8bd103df33693ba2a51449ac637a4b7274b
silentfl <silentfl@mail.ru>
2017-02-23 22:34:06 +0300

    Fix Time job params (Time not serialized)

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -2,8 +2,8 @@ module Api::V1
   class ReportsController < ApiController
     def by_author
       now = Time.now
-      start_date = params[:start_date] || now - 1.month
-      end_date = params[:end_date] || now
+      start_date = Time.parse(params[:start_date]) rescue now - 1.month
+      end_date = Time.parse(params[:end_date]) rescue now
       email = params[:email]
 
       unless email.present?
@@ -15,7 +15,7 @@ module Api::V1
       end_date = now if end_date > now
       start_date = end_date - 1.day if start_date > end_date
 
-      CreateReportJob.perform_later(start_date, end_date, email)
+      CreateReportJob.perform_later(email, start_date.to_s, end_date.to_s)
     end
   end
 end
--- app/jobs/create_report_job.rb
+++ app/jobs/create_report_job.rb
@@ -1,7 +1,13 @@
 class CreateReportJob < ApplicationJob
   queue_as :default
 
-  def perform(start_date, end_date, email)
+  def perform(email, start_date_string, end_date_string)
+    start_date = Time.parse(start_date_string)
+    end_date = Time.parse(end_date_string)
+
+    # example data, no date filter
     report = User.all.map { |u| { nickname: u.nickname, email: u.email, posts: u.posts.size, comments: u.comments.size } }
+
+    ReportMailer.send_report(email, report, start_date_string, end_date_string).deliver_later
   end
 end
